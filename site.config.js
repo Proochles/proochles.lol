08613d80419c37b1030b94cf639e5ea00d84e922
Proochles <me@proochles.lol>
2024-05-27 02:19:00 -0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,27 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "proochles",
+    name: "morethanmin",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "moderation consultant",
-    bio: "moderation and management are my thing",
-    email: "me@proochles.lol",
+    role: "frontend developer",
+    bio: "I develop everything using node.",
+    email: "morethanmin.dev@gmail.com",
+    linkedin: "morethanmin",
+    github: "morethanmin",
+    instagram: "",
   },
   projects: [
     {
-      name: `placeholder`,
-      href: "https://example.com",
+      name: `morethan-log`,
+      href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "proochles.lol",
-    description: "welcome to proochles' dungeon!",
+    title: "morethan-log",
+    description: "welcome to morethan-log!",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2024, // If leave this empty, current year will be used.
+  since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
@@ -29,7 +32,6 @@ const CONFIG = {
   notionConfig: {
     pageId: process.env.NOTION_PAGE_ID,
   },
-
   // plugin configuration (optional)
   googleAnalytics: {
     enable: false,
@@ -67,5 +69,4 @@ const CONFIG = {
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
-
 module.exports = { CONFIG }
