a516c57dff162470f63462eabb0601e8f742674a
Proochles <me@proochles.lol>
2024-05-27 03:39:55 -0700

    test 2

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -51,9 +51,9 @@ export default Feed
 const StyledWrapper = styled.div`
   grid-template-columns: repeat(12, minmax(0, 1fr));
 
-  padding: 7rem 0;
+  padding: 2rem 0;
   display: grid;
-  gap: 1.5rem;
+  gap: 3rem;
 
   @media (max-width: 768px) {
     display: block;
