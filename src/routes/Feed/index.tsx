063f15817a6c4dcfc19a72288b40653e88f3ab49
Proochles <me@proochles.lol>
2024-05-27 03:29:39 -0700

    accidentlly duped the profile card.

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -26,9 +26,6 @@ const Feed: React.FC<Props> = () => {
           height: `calc(100vh - ${HEADER_HEIGHT}px)`,
         }}
       >
-        <ProfileCard />
-        <ServiceCard />
-        <ContactCard />
         <TagList />
       </div>
       <div className="mid">
