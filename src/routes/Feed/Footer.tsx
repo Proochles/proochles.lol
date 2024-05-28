70263de64f6e8129d5f207f25c6b3d303a289c2d
Proochles <me@proochles.lol>
2024-05-27 19:21:12 -0700

    footer stuff

--- src/routes/Feed/Footer.tsx
+++ src/routes/Feed/Footer.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC<Props> = ({ className }) => {
   return (
     <StyledWrapper className={className}>
       <a
-        href={`https://github.com/${CONFIG.profile.github}`}
+        href={`https://x.com/${CONFIG.profile.x}`}
         target="_blank"
         rel="noreferrer"
       >
